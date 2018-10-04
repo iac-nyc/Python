d4b7e4bf9bc6407ed3e82ec3701762882dbd031a
iac-nyc <007ifte@gmail.com>
2018-10-04 09:15:35 -0400

    magic_word

---
+++ magic_word.py
@@ -0,0 +1,17 @@
+request = input("What would you like?  ")
+while "please" not in request:
+    request = input("You seem to be missing a magic word.  What would you like?  ")
+print("Here you go!")
+
+
+#####
+banner = "Happy Birthday!";
+
+for letter in banner:
+    print(letter.upper());
+    
+###
+
+for letter in "You got this!":
+    if letter in "oh":
+        print(letter)
